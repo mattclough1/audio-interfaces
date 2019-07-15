57631d53611c8e55ea734bc23251f27d70e20c45
Matt Clough <matt.clough@mobelux.com>
2019-07-15 12:01:42 -0400

    working, refactored to use explicit callbacks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
 import SpeechContext from './contexts/SpeechContext';
-import Prompter from './components/Prompter';
+import Prompter from './components/Prompter/';
 import MockVoiceInput from './components/MockVoiceInput';
+
+const Cue = ({ children }) => {
+  return <span>children</span>;
+};
+
+function changeColor(bgColor, textColor) {
+  document.body.style.backgroundColor = bgColor;
+  document.body.style.color = textColor;
+}
 
 function App() {
   const [speechTranscript, setSpeechTranscript] = useState('');
 
-  const triggerMap = {
-    changeColor: (bgColor, textColor) => {
-      console.log(bgColor, textColor);
-      document.body.style.backgroundColor = bgColor;
-      document.body.style.color = textColor;
-    },
-  };
-
   return (
     <div className="App">
       <SpeechContext.Provider value={{ speechTranscript, setSpeechTranscript }}>
-        <Prompter callbacks={triggerMap}>
-          This is a test. If I say the word blue[changeColor('#73e1ff', '#ffffff')] the background
-          should change blue, and if I say the word pink[changeColor('#ffb3c4', '#ffffff')] the
-          background should change to pink
+        <Prompter>
+          This is a test. Let's change the background
+          <Cue
+            callback={() => {
+              changeColor('#fce77d', '#f96167');
+            }}
+          >
+            color
+          </Cue>
+          . In fact, let's make this speech a bit longer and do some more things later on. But first
+          let's describe some tech things, and once we're done with that, we'll change the
+          background color <Cue callback={() => changeColor('#fbeaeb', '#2f3c7e')}>again</Cue>
         </Prompter>
         <MockVoiceInput />
       </SpeechContext.Provider>
